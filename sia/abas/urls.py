c68f82e652f994d44fd8d4e27114d5c5f80b3891
yury <yuresz_9@hotmail.com>
2013-08-22 12:42:40 -0500

    lista areas

--- sia/abas/urls.py
+++ sia/abas/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('abas.views',
   url(r'^requerimientos/json$', 'lista_requerimientos_json', name = 'lista_requerimientos_json'),
+  url(r'^areas/json$', 'lista_areas_json', name = 'lista_areas_json'),
 )
